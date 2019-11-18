b2abd910814d36e17b910091c496679d8b95396f
KasperSiig <kasp9250@easv365.dk>
2019-11-18 19:26:37 +0100

    Added - Router and Job in Store module

---
+++ src/app/shared/store/router/router-state.serializer.ts
@@ -0,0 +1,26 @@
+import { RouterStateModel } from './router-state.model';
+import { RouterStateSerializer } from '@ngxs/router-plugin';
+import { RouterStateSnapshot } from '@angular/router';
+
+export class CustomRouterStateSerializer
+	implements RouterStateSerializer<RouterStateModel> {
+	serialize(routerState: RouterStateSnapshot): RouterStateModel {
+		const {
+			url,
+			root: { queryParams },
+		} = routerState;
+
+		let { root: route } = routerState;
+		while (route.firstChild) {
+			route = route.firstChild;
+		}
+
+		const { params, data } = route;
+
+		if (route.params.title) {
+			data.title = route.params.title;
+		}
+
+		return { url, params, queryParams, data };
+	}
+}
