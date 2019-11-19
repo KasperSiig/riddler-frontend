07e90372c75af256783760e24252da0b6fcaa80f
KasperSiig <kasp9250@easv365.dk>
2019-11-19 14:28:24 +0100

    Added - Header and tabs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,9 @@ import { Component } from '@angular/core';
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+	tabs = [
+		{ title: 'Jobs', url: 'jobs' },
+		{ title: 'Wordlists', url: 'wordlists' },
+	];
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from './shared/store/store.module';
 
 @NgModule({
 	declarations: [AppComponent],
-	imports: [BrowserModule, AppRoutingModule, StoreModule],
+	imports: [BrowserModule, AppRoutingModule, StoreModule, MatToolbarModule],
 	providers: [],
 	bootstrap: [AppComponent],
 })
