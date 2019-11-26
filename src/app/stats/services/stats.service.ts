700496f9b1afaca5406b128db088293ed7ef9f2b
KasperSiig <kasp9250@easv365.dk>
2019-11-26 13:03:30 +0100

    Added - Get stats from backend

--- src/app/stats/services/stats.service.ts
+++ src/app/stats/services/stats.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { of } from 'rxjs';
+import { of, Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
 	providedIn: 'root',
@@ -9,11 +10,33 @@ import { of } from 'rxjs';
 export class StatsService {
 	constructor(private http: HttpClient) {}
 
-	getAdminsCracked(id: string) {
-		return of({
-			total: 500,
-			cracked: 250,
-			percentage: 50,
-		});
+	/**
+	 * Gets stats on how many admin accounts are cracked
+	 *
+	 * @param id Id of job to get stats on
+	 */
+	getAdminsCracked(
+		id: string,
+	): Observable<{ total: number; cracked: number; percentage: number }> {
+		return this.http.get<{
+			total: number;
+			cracked: number;
+			percentage: number;
+		}>(environment.apiUrl + id + '/admins');
+	}
+
+	/**
+	 * Gets stats on how many user accounts are cracked
+	 *
+	 * @param id Id of job to get stats on
+	 */
+	getAllCracked(
+		id: string,
+	): Observable<{ total: number; cracked: number; percentage: number }> {
+		return this.http.get<{
+			total: number;
+			cracked: number;
+			percentage: number;
+		}>(environment.apiUrl + id + '/all');
 	}
 }
--- src/app/stats/stats.component.spec.ts
+++ src/app/stats/stats.component.spec.ts
@@ -1,25 +1,55 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { StatsComponent } from './stats.component';
+import { StoreModule } from '../shared/store/store.module';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Store } from '@ngxs/store';
+import { RouterSelectors } from '../shared/store/router/router.selectors';
+import { Navigate } from '@ngxs/router-plugin';
+import { Router } from '@angular/router';
 
 describe('StatsComponent', () => {
-  let component: StatsComponent;
-  let fixture: ComponentFixture<StatsComponent>;
+	let component: StatsComponent;
+	let fixture: ComponentFixture<StatsComponent>;
+	let store: Store;
+	let router: Router;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ StatsComponent ]
-    })
-    .compileComponents();
-  }));
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			declarations: [StatsComponent],
+			imports: [
+				StoreModule,
+				RouterTestingModule.withRoutes([
+					{
+						path: '',
+						pathMatch: 'full',
+						redirectTo: 'stats/test',
+					},
+					{
+						path: 'stats/:id',
+						component: StatsComponent,
+					},
+				]),
+			],
+		}).compileComponents();
+	}));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(StatsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		store = TestBed.get<Store>(Store);
+		router = TestBed.get(Router);
+		fixture = TestBed.createComponent(StatsComponent);
+		component = fixture.componentInstance;
+		store.reset({
+			router: {
+				state: {
+					params: { id: 'test' },
+				},
+			},
+		});
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
