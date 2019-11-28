4358f4875e492c875c75285d2deed13896f6ec57
MissTizz <tizz3n@hotmail.com>
2019-11-28 14:48:49 +0100

    Finished test
    Tested what happens when you click 2 times.

--- src/app/job/jobs/jobs.component.spec.ts
+++ src/app/job/jobs/jobs.component.spec.ts
@@ -11,6 +11,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { filterStackTrace } from 'protractor/built/util';
 
 describe('JobsComponent', () => {
 	let component: JobsComponent;
@@ -59,5 +60,15 @@ describe('JobsComponent', () => {
 		const status = el.querySelectorAll('app-status');
 
 		expect(status.length).toBe(2);
+	});
+
+	it('should sort on click', () => {
+		const el: HTMLElement = fixture.debugElement.nativeElement;
+		const button: HTMLElement = el.querySelector('.jobtitle__name');
+		expect( component.sorting).toEqual('time');
+		button.click();
+		expect(component.sorting).toEqual('name');
+		button.click();
+		expect(component.sorting).toEqual('namereversed');
 	});
 });
