2a83decad0d7b4b39d832b55f8a31e001c7c7b14
KasperSiig <kasp9250@easv365.dk>
2019-12-05 13:10:50 +0100

    Fixed - References to wordlist

--- src/app/job/status/status.component.spec.ts
+++ src/app/job/status/status.component.spec.ts
@@ -59,7 +59,7 @@ describe('Status Component', () => {
 		job = {
 			_id: 'test',
 			name: 'Sommer2019',
-			wordlist: 'ripper1',
+			wordlist: { name: 'wordlist', path: '/opt/jtr/wordlist.txt' },
 			status: STATUS.STARTED,
 			time: new Date(2020, 1, 1).getMinutes(),
 		};
@@ -92,7 +92,7 @@ describe('Status Component', () => {
 
 		const el: HTMLElement = testHostFixture.debugElement.nativeElement;
 		const name = el.querySelector('.job__wordlist');
-		expect(name.textContent.trim()).toEqual(job.wordlist);
+		expect(name.textContent.trim()).toEqual(job.wordlist.name);
 	});
 
 	it('should contain a date', () => {
